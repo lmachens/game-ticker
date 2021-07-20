9399c5534117f2fe891512f235aa9d2114951436
lmachens <leon@machens.koeln>
2021-07-20 14:36:07 +0200

    Restructure helper functions

--- src/app/utils/highlights.ts
+++ src/app/utils/highlights.ts
@@ -1,57 +1,72 @@
-// Todo:
-// 1) Promisfy to reduce nesting (callback hell)
-// 2) turnOf capturing on game end
-// 3) Record highlights if app is started after a game
-
-// League of Legends Config
-const GAME_ID = 5426;
-
-export async function getHighlights(
-  id: number
-): Promise<string[] | undefined | null> {
+export function getHighlightsFeatures(id: number): Promise<string[]> {
   return new Promise((resolve, reject) => {
     overwolf.media.replays.getHighlightsFeatures(id, (event) => {
       console.log('getHighlightsFeatures', event);
       if (event.success) {
-        resolve(event.features);
+        resolve(event.features!);
+      } else {
+        reject(event.error);
       }
-      reject(null);
     });
   });
 }
 
-export async function turnOnReplay(
-  highlights: string[] | null | undefined
-): Promise<overwolf.media.replays.TurnOnResult | null> {
-  const requiredHighlights =
-    highlights && highlights.length ? highlights : ['*'];
-
+export function turnOnReplay(
+  requiredHighlights: string[]
+): Promise<overwolf.media.replays.TurnOnResult> {
   return new Promise((resolve, reject) => {
-    if (highlights) {
-      overwolf.media.replays.turnOn(
-        {
-          settings: {
-            video: {
-              buffer_length: 30000,
-            },
-          },
-          highlights: {
-            enable: true,
-            requiredHighlights,
+    overwolf.media.replays.turnOn(
+      {
+        settings: {
+          video: {
+            buffer_length: 30000,
           },
         },
-        (event) => {
-          if (event.success) {
-            console.log('turned on', event);
-            resolve(event);
-          } else {
-            console.log('replay failed', event);
-            reject(null);
-          }
+        highlights: {
+          enable: true,
+          requiredHighlights,
+        },
+      },
+      (event) => {
+        console.log('turnOn', event);
+        if (event.success) {
+          resolve(event);
+        } else {
+          reject(event.error);
         }
-      );
-    }
+      }
+    );
   });
+}
+
+export function turnOffReplayIfOn(): Promise<void> {
+  return new Promise((resolve, reject) => {
+    overwolf.media.replays.getState((state) => {
+      if (!state.success) {
+        reject(state.error);
+        return;
+      }
+      if (!state.isOn) {
+        resolve();
+        return;
+      }
+      overwolf.media.replays.turnOff((event) => {
+        console.log('turnOff', event);
+        if (event.success) {
+          resolve();
+        } else {
+          reject(event.error);
+        }
+      });
+    });
+  });
+}
+
+async function getHighlightsAndTurnOn(classId: number) {
+  const highlights = await getHighlightsFeatures(classId);
+  if (highlights.length) {
+    await turnOnReplay(highlights);
+  }
 }
 
 export function startCaptureHighlights(): void {
@@ -72,39 +87,22 @@ export function startCaptureHighlights(): void {
     console.log('onHighlightsCaptured', event);
   });
 
+  overwolf.games.onGameLaunched.addListener(async (event) => {
+    console.log('onGameLaunched', event);
+    await getHighlightsAndTurnOn(event.classId);
+  });
+
   overwolf.games.getRunningGameInfo(async (result) => {
-    console.log('check if game is already running');
-    if (!result) {
-      overwolf.games.onGameLaunched.addListener(async (event) => {
-        console.log('onGameLaunched', event);
-
-        if (event.classId === GAME_ID) {
-          const highlights = await getHighlights(GAME_ID);
-          await turnOnReplay(highlights);
-        }
-      });
-    }
-
-    const classId =
-      !Array.isArray(result) && result?.classId ? result.classId : null;
-
-    if (classId === GAME_ID) {
-      console.log('game already running');
-      const highlights = await getHighlights(GAME_ID);
-      await turnOnReplay(highlights);
+    console.log('check if game is already running', result);
+    if (result) {
+      await getHighlightsAndTurnOn(result.classId);
     }
   });
 
-  overwolf.games.onGameInfoUpdated.addListener((event) => {
-    console.log('game info changed', event.gameInfo);
-    if (!event.gameInfo?.isRunning) {
-      overwolf.media.replays.turnOff((event) => {
-        if (event.success) {
-          console.log('replay turned off', event);
-        } else {
-          console.log('replay turnOff error', event.error);
-        }
-      });
+  overwolf.games.onGameInfoUpdated.addListener(async (event) => {
+    console.log('game info changed', event);
+    if (event.runningChanged && !event.gameInfo!.isRunning) {
+      await turnOffReplayIfOn();
     }
   });
 }
