880e407984d42b9429310867c0807d06edf72e5b
lmachens <leon@machens.koeln>
2021-07-21 14:05:45 +0200

    Prepare POST /matches

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -2,8 +2,34 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/hello', async (_req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+type Match = {
+  _id?: string;
+  gameId: number;
+  username: string;
+  createdAt: Date;
+  highlights: {
+    timestamp: number;
+    type: string;
+    videoSrc: string;
+  }[];
+};
+
+router.post('/matches', async (req, res) => {
+  const { username, gameId } = req.body;
+
+  if (typeof username !== 'string' || typeof gameId !== 'number') {
+    res.status(400).send('Invalid payload');
+    return;
+  }
+
+  const match: Match = {
+    gameId: gameId,
+    username: username,
+    createdAt: new Date(),
+    highlights: [],
+  };
+
+  res.status(200).json(match);
 });
 
 export default router;
