3da86ad4e572909b89013da1307e84256d07f419
lmachens <leon@machens.koeln>
2021-07-22 16:43:37 +0200

    Optimize query by adding indexes

--- src/lib/matches.ts
+++ src/lib/matches.ts
@@ -18,3 +18,12 @@ export type Match = {
 export function getMatchesCollection(): Collection<Match> {
   return getCollection<Match>('matches');
 }
+
+export function ensureMatchesIndexes(): Promise<string[]> {
+  return getMatchesCollection().createIndexes([
+    { key: { createdAt: -1 } },
+    { key: { username: 1 } },
+    { key: { gameId: 1 } },
+    { key: { gameId: 1, username: 1 } },
+  ]);
+}
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import compression from 'compression';
 import { connectToMongoDb } from './lib/db';
 import router from './lib/router';
 import helmet from 'helmet';
+import { ensureMatchesIndexes } from './lib/matches';
 
 const { PORT, MONGODB_URI } = process.env;
 
@@ -34,8 +35,10 @@ app.get('*', (_req, res) => {
   res.send('<h1>Welcome to the server</h1>');
 });
 
-connectToMongoDb(MONGODB_URI).then(() => {
+connectToMongoDb(MONGODB_URI).then(async () => {
   console.log('Connected to MongoDB');
+  await ensureMatchesIndexes();
+
   app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
   });
