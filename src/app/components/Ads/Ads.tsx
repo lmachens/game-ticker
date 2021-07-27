181c46dbff9b534cb4707d7ab52dc4a7646e0813
sschoe85 <s@bine-schoetensack.eu>
2021-07-27 12:20:47 +0200

    ✨ the ad component now reacts to user minimizing and maximizing the window

--- src/app/components/Ads/Ads.tsx
+++ src/app/components/Ads/Ads.tsx
@@ -1,17 +1,21 @@
-import { useEffect, useRef } from 'react';
+import { OwAd } from '@overwolf/types/owads';
+import { useEffect, useRef, useState } from 'react';
+import { getCurrentWindow } from '../../utils/windows';
 import classes from './Ads.module.css';
 
 function Ads(): JSX.Element {
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const [owAd, setOwAd] = useState<OwAd>();
 
   useEffect(() => {
     function onOwAdReady() {
       if (typeof window.OwAd === 'undefined' || containerRef.current === null) {
         return;
       }
-      new window.OwAd(containerRef.current, {
+      const ad = new window.OwAd(containerRef.current, {
         size: { width: 400, height: 300 },
       });
+      setOwAd(ad);
     }
     const script = document.createElement('script');
     script.src = 'https://content.overwolf.com/libs/ads/latest/owads.min.js';
@@ -22,6 +26,34 @@ function Ads(): JSX.Element {
       document.body.removeChild(script);
     };
   }, []);
+
+  useEffect(() => {
+    if (!owAd) {
+      return;
+    }
+    async function onWindowStateChanged(
+      state: overwolf.windows.WindowStateChangedEvent
+    ) {
+      const currentWindow = await getCurrentWindow();
+      if (currentWindow.id !== state.window_id) {
+        return;
+      } else {
+        console.log(`Window state changed:' ${JSON.stringify(state)}`);
+        if (state.window_state_ex === 'minimized') {
+          owAd?.removeAd();
+        } else if (
+          state.window_previous_state_ex === 'minimized' &&
+          state.window_state_ex === 'normal'
+        ) {
+          owAd?.refreshAd({});
+        }
+      }
+    }
+    overwolf.windows.onStateChanged.addListener(onWindowStateChanged);
+    return () => {
+      overwolf.windows.onStateChanged.removeListener(onWindowStateChanged);
+    };
+  }, [owAd]);
   return <div className={classes.container} ref={containerRef} />;
 }
 
