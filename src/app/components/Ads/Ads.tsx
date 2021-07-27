4637cbbec95ea39d893a79c45b1ac2de22f3c0af
sschoe85 <s@bine-schoetensack.eu>
2021-07-27 10:05:40 +0200

    🎨 rename variables

--- src/app/components/Ads/Ads.tsx
+++ src/app/components/Ads/Ads.tsx
@@ -2,17 +2,14 @@ import { useEffect, useRef } from 'react';
 import classes from './Ads.module.css';
 
 function Ads(): JSX.Element {
-  const adsContainerRef = useRef<HTMLDivElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     function onOwAdReady() {
-      if (
-        typeof window.OwAd === 'undefined' ||
-        adsContainerRef.current === null
-      ) {
+      if (typeof window.OwAd === 'undefined' || containerRef.current === null) {
         return;
       }
-      new window.OwAd(adsContainerRef.current, {
+      new window.OwAd(containerRef.current, {
         size: { width: 400, height: 300 },
       });
     }
@@ -25,7 +22,7 @@ function Ads(): JSX.Element {
       document.body.removeChild(script);
     };
   }, []);
-  return <div className={classes.adsContainer} ref={adsContainerRef}></div>;
+  return <div className={classes.container} ref={containerRef} />;
 }
 
 export default Ads;
