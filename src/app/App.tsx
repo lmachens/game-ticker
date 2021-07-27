7c02855a9089542a67b454ac8c9630daec2ab42c
lmachens <leon@machens.koeln>
2021-07-27 11:05:11 +0200

    ✨ Display match details

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react';
-import styles from './App.module.css';
+import classes from './App.module.css';
 import AppHeader from './components/AppHeader/AppHeader';
 import Feed from './components/Feed/Feed';
 import type { Match } from '../types';
+import MatchDetails from './components/MatchDetails/MatchDetails';
 
 function App(): JSX.Element {
   const [match, setMatch] = useState<Match | null>(null);
   return (
-    <div className={styles.container}>
-      <AppHeader className={styles.header} />
-      <main className={styles.main}>
-        <Feed onMatchClick={setMatch} />
+    <div className={classes.container}>
+      <AppHeader className={classes.header} />
+      <main className={classes.main}>
+        {match ? (
+          <>
+            <button className={classes.back} onClick={() => setMatch(null)}>
+              &lt;- Back to feed
+            </button>
+            <MatchDetails match={match} />
+          </>
+        ) : (
+          <Feed onMatchClick={setMatch} />
+        )}
       </main>
-      <aside className={styles.aside}>ASIDE</aside>
+      <aside className={classes.aside}>ASIDE</aside>
     </div>
   );
 }
