075fc61c168fcab429fecddca2bdfc653e107212
AlexisRoe <A.Roehrling@gmx.de>
2021-07-27 15:31:44 +0200

    ✨ Create summary item for ingame window

---
+++ src/app/components/SummaryHighlight/SummaryHighlight.examples.tsx
@@ -0,0 +1,6 @@
+import { Example } from '../examples';
+import SummaryHighlightItem from './SummaryHighlight';
+
+export const LolSummaryHighlightItem: Example = () => (
+  <SummaryHighlightItem highlights={['test, test']} timestamp="1627390756" />
+);
