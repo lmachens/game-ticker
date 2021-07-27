4e31087e942ec4846c302e6ec18a9e88934a641c
lmachens <leon@machens.koeln>
2021-07-27 10:19:41 +0200

    💄 Add main layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,14 @@
 import styles from './App.module.css';
+import AppHeader from './components/AppHeader/AppHeader';
 
 function App(): JSX.Element {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <AppHeader className={styles.header} />
+      <main className={styles.main}>MAIN</main>
+      <aside className={styles.aside}>ASIDE</aside>
+    </div>
+  );
 }
 
 export default App;
