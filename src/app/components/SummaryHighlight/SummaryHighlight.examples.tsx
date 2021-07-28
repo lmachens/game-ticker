533488f77e5fe9a1e0d48a71f0e4eef87bd9f743
AlexisRoe <A.Roehrling@gmx.de>
2021-07-28 14:58:23 +0200

    🔥 Remove old code version

--- src/app/components/SummaryHighlight/SummaryHighlight.examples.tsx
+++
@@ -1,6 +0,0 @@
-import { Example } from '../examples';
-import SummaryHighlightItem from './SummaryHighlight';
-
-export const LolSummaryHighlightItem: Example = () => (
-  <SummaryHighlightItem highlights={['test, test']} timestamp="1627390756" />
-);
--- src/app/components/SummaryHighlightItem/SummaryHighlightItem.examples.tsx
+++ src/app/components/SummaryHighlightItem/SummaryHighlightItem.examples.tsx
@@ -2,5 +2,5 @@ import { Example } from '../examples';
 import SummaryHighlightItem from './SummaryHighlightItem';
 
 export const SummaryHighlightsItem: Example = () => (
-  <SummaryHighlightItem events={['test, test']} timestamp={145} />
+  <SummaryHighlightItem events={['kill', 'assist']} timestamp={145} />
 );
