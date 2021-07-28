173db8ef44e04d0e60a1581badb3d895f206cc53
lmachens <leon@machens.koeln>
2021-07-28 10:14:08 +0200

    ✨ Add match details API endpoint

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -39,6 +39,29 @@ router.post('/matches', async (request, response, next) => {
   }
 });
 
+router.get('/matches/:matchId', async (request, response, next) => {
+  try {
+    const { matchId } = request.params;
+    const matchesCollection = await getMatchesCollection();
+    const match = await matchesCollection.findOne({
+      _id: new ObjectId(matchId),
+    });
+    if (!match) {
+      response.status(404).send('Match not found');
+      return;
+    }
+    response.status(200).json(match);
+  } catch (error) {
+    if (error instanceof MongoServerError) {
+      console.error(error);
+      response.setHeader('Content-Type', 'plain/text');
+      response.status(500).send(error.message);
+      return;
+    }
+    next(error);
+  }
+});
+
 router.post('/matches/:matchId/highlights', async (request, response, next) => {
   try {
     const { timestamp, events, videoSrc } = request.body;
