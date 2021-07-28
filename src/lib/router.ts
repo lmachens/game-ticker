772e8e626f44e108fc05cd507feaf74f3fb3254d
lmachens <leon@machens.koeln>
2021-07-28 16:33:22 +0200

    🔥 Remove specific error catch

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -29,12 +29,6 @@ router.post('/matches', async (request, response, next) => {
     }
     response.status(200).json(match);
   } catch (error) {
-    if (error instanceof MongoServerError) {
-      console.error(error);
-      response.setHeader('Content-Type', 'plain/text');
-      response.status(500).send(error.message);
-      return;
-    }
     next(error);
   }
 });
@@ -58,12 +52,6 @@ router.get('/matches/:matchId', async (request, response, next) => {
     }
     response.status(200).json(match);
   } catch (error) {
-    if (error instanceof MongoServerError) {
-      console.error(error);
-      response.setHeader('Content-Type', 'plain/text');
-      response.status(500).send(error.message);
-      return;
-    }
     next(error);
   }
 });
@@ -109,12 +97,6 @@ router.post('/matches/:matchId/highlights', async (request, response, next) => {
     }
     response.status(200).json(updatedMatch);
   } catch (error) {
-    if (error instanceof MongoServerError) {
-      console.error(error);
-      response.setHeader('Content-Type', 'plain/text');
-      response.status(500).send(error.message);
-      return;
-    }
     next(error);
   }
 });
