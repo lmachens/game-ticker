301fd131114899d3c2ab0bcbf9f0b83e76246a54
AlexisRoe <A.Roehrling@gmx.de>
2021-07-30 12:32:43 +0200

    🎨 Externalize Gradient into own component

--- src/app/components/IconSvg/IconSvg.tsx
+++ src/app/components/IconSvg/IconSvg.tsx
@@ -25,22 +25,6 @@ function IconSvg({
         disabled && classes.disabled
       )}
     >
-      <defs>
-        <linearGradient
-          id="occtyGradient"
-          x1="0.94"
-          y1="0.74"
-          x2="0.06"
-          y2="0.26"
-        >
-          <stop offset="0%" stopColor="#fe804b" />
-          <stop offset="10.67%" stopColor="#fb8d28" />
-          <stop offset="32%" stopColor="#ff8f38" />
-          <stop offset="42.67%" stopColor="#ff824f" />
-          <stop offset="64%" stopColor="#fe656f" />
-          <stop offset="100%" stopColor="#fb31b3" />
-        </linearGradient>
-      </defs>
       {children}
     </svg>
   );
