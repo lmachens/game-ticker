9a19d1064a178b8a744e52acf2b3deb99f331082
webdevelukas <lukas@deinklub.de>
2021-07-30 12:36:46 +0200

    :recycle: modify match item due to actual changes

--- src/app/components/MatchItem/MatchItem.tsx
+++ src/app/components/MatchItem/MatchItem.tsx
@@ -1,5 +1,7 @@
 import { MatchClient } from '../../../types';
+import useFetch from '../../hooks/useFetch';
 import useGameInfo from '../../hooks/useGameInfo';
+import { getHighlights } from '../../utils/api';
 import classes from './MatchItem.module.css';
 
 type MatchItemProps = {
@@ -8,12 +10,15 @@ type MatchItemProps = {
 };
 function MatchItem({ match, onClick }: MatchItemProps): JSX.Element {
   const gameInfo = useGameInfo(match.gameId);
+  const { data: highlights } = useFetch(() =>
+    getHighlights({ page: 1, itemsPerPage: 10, matchId: match._id })
+  );
 
   return (
     <article className={classes.container} onClick={onClick}>
       <p>{match.username}</p>
       <p>{gameInfo?.Label}</p>
-      <p>{match.highlights.length}</p>
+      {highlights && <p>{highlights.results.length}</p>}
     </article>
   );
 }
