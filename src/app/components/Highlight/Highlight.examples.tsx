b585945e43d0c66ccb85408145ff7f77d5fb1b34
lmachens <leon@machens.koeln>
2021-07-30 09:29:09 +0200

    ✨ Add Highlight component

--- src/app/components/Highlight/Highlight.examples.tsx
+++ src/app/components/Highlight/Highlight.examples.tsx
@@ -1,6 +1,22 @@
 import { Example } from '../examples';
 import Highlight from './Highlight';
 
-export const HighlightItem: Example = () => (
-  <Highlight events={['kill', 'assist']} timestamp={145} />
+export const HighlightFullLayout: Example = () => (
+  <Highlight
+    matchIsActive={true}
+    layout="full"
+    events={['kill', 'assist']}
+    timestamp={1627569057364}
+    videoSrc="overwolf://media/replays/test.mp4"
+  />
+);
+
+export const HighlightHalfLayout: Example = () => (
+  <Highlight
+    matchIsActive={false}
+    layout="half"
+    events={['death']}
+    timestamp={1627569167364}
+    videoSrc="overwolf://media/replays/test.mp4"
+  />
 );
--- src/app/components/Highlight/Highlight.tsx
+++ src/app/components/Highlight/Highlight.tsx
@@ -1,33 +1,65 @@
+import { MatchHighlight } from '../../../types';
+import { classNames } from '../../utils/styles';
+import VideoHighlight from '../VideoHighlight/VideoHighlight';
 import classes from './Highlight.module.css';
-
-function formatSeconds(seconds: number): string {
-  if (seconds < 10) {
-    return `0${seconds}`;
-  }
-
-  return `${seconds}`;
-}
-
-function translateSecondsToTimeString(timestamp: number): string {
-  const duration = Number(timestamp);
-  const minutes = Math.floor((duration % 3600) / 60);
-  const seconds = Math.floor((duration % 3600) % 60);
-
-  const minutesDisplay = minutes > 0 ? minutes : '0';
-  const secondsDisplay = formatSeconds(seconds);
-  return `${minutesDisplay}:${secondsDisplay}`;
-}
+import commentSrc from './comment.png';
+import shareSrc from './share.png';
+import likeSrc from './like.png';
+import { toTimeAgo } from '../../utils/dates';
 
 type HighlightProps = {
-  events: string[];
-  timestamp: number;
-};
+  matchIsActive: boolean;
+  layout: 'full' | 'half';
+} & MatchHighlight;
 
-function Highlight({ events, timestamp }: HighlightProps): JSX.Element {
+function Highlight({
+  matchIsActive,
+  layout,
+  timestamp,
+  videoSrc,
+}: HighlightProps): JSX.Element {
   return (
-    <article className={classes.container}>
-      <p>{events.join(', ')}</p>
-      <p>{translateSecondsToTimeString(timestamp)}</p>
+    <article className={classNames(classes.container, classes[layout])}>
+      <header className={classes.header}>
+        <section>PROFILE</section>
+        <section className={classes.info}>
+          {layout === 'full' && (
+            <p>
+              {matchIsActive && (
+                <div className={classes.info__active}>
+                  <svg width={12} height={12}>
+                    <circle fill="green" r="6" cx="6" cy="6" />
+                  </svg>
+                  Match is active
+                </div>
+              )}
+            </p>
+          )}
+
+          <p>{toTimeAgo(new Date(timestamp))}</p>
+        </section>
+      </header>
+      <main>
+        <VideoHighlight src={videoSrc} />
+      </main>
+      <aside className={classes.more}>
+        <button className={classes.social}>
+          <img src={commentSrc} alt="Comments" />
+          20
+        </button>
+        <button className={classes.social}>
+          <img src={shareSrc} alt="Share" />5
+        </button>
+        <button className={classes.social}>
+          <img src={likeSrc} alt="Likes" />
+          360
+        </button>
+        {layout === 'full' && (
+          <a className={classes.more__match} href="#">
+            Go to match
+          </a>
+        )}
+      </aside>
     </article>
   );
 }
